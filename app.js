4d6c9ffabaa91f6a45e4645f11c92f8f2e542ef4
taddeimania <jtaddei@gmail.com>
2017-07-05 10:43:04 -0400

    started script

--- app.js
+++ app.js
@@ -1,2 +1,10 @@
 
 const models = require('./models');
+
+models.Blog.findById(2).then(blog => {
+  // console.log(blog.title);
+  blog.getTags().then(tags => {
+    console.log(tags);
+  });
+  // models.sequelize.close();  // NEVER USE IN EXPRESS!!!!!!!@!!!!!!1
+})
